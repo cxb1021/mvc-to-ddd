51dc9d904e8d5a6ea9787cb2867ca05463973748
cxb1021 <caoxianboscu@163.com>
2024-06-20 12:45:23 +0800

    update ddd

--- src/main/java/org/lv/domain/WorkExperience.java
+++ src/main/java/org/lv/domain/WorkExperience.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import org.lv.common.framework.ValueObject;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class WorkExperience implements ValueObject {
     @JsonFormat(pattern = "yyyy-MM-dd")
